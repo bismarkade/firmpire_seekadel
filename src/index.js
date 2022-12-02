0f8f768f49f5f8780a93d2e5e018d029d8681dca
Bismark Ade <bismarkade@gmail.com>
2022-12-02 18:27:23 +0100

    file and folder structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
-// ReactDOM.render(< App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        < App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App/>)
 
