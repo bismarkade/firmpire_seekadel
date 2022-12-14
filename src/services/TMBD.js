4400696f088083f1e77435dc0ad73d7719270528
Bismark Ade <bismarkade@gmail.com>
2022-12-14 22:29:22 +0100

    implemented switch gener functionality

--- src/services/TMBD.js
+++ src/services/TMBD.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const  tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
 // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 
-const page = 1;
+// const page = 1;
 
 export const tmdbApi = createApi({
     // every create api call must have:
@@ -24,7 +24,20 @@ export const tmdbApi = createApi({
         // * Get Movies by [type]
         getMovies: builder.query({
             // get popular movies
-            query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+            query: ({ genreIdOrCategoryName, page }) => {
+
+                //* Get Movies by Cartegory (popular, top_rated, upcoming -> string)
+                if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string'){
+                    return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+                }
+                //* Get Movies by Genre (12, 15 -> number)
+                if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number'){
+                    return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+                }
+
+                 //* Get Popular movies
+               return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+            },
         }),
     }),
 });
