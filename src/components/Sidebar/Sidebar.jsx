2252c28229fd0d79bbae18742dde4997c8aa3b63
Bismark Ade <bismarkade@gmail.com>
2022-12-15 00:41:00 +0100

    update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -19,12 +19,12 @@ const categories = [
 ];
 
 // Mock of all the categories
-const demoCategories = [
-  {label: 'Comedy', value: 'comedy'},
-  {label: 'Action', value: 'action'},
-  {label: 'Horror', value: 'horro'},
-  {label: 'Animation', value: 'animation'},
-];
+// const demoCategories = [
+//   {label: 'Comedy', value: 'comedy'},
+//   {label: 'Action', value: 'action'},
+//   {label: 'Horror', value: 'horro'},
+//   {label: 'Animation', value: 'animation'},
+// ];
 
 const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
 const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
