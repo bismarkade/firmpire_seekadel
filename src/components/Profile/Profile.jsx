507f60f7ba47997734b3ebf9371ebf2abbb3d987
Bismark Ade <bismarkade@gmail.com>
2022-12-15 22:35:56 +0100

    implemented the logout function in the Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Typography, Button, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 import { setUser, userSelector } from '../../features/auth';
 
 const Profile = () => { 
   const { user } = useSelector(userSelector);
 
+  const favoriteMovies = [];
+
+  const logout = () => {
+      localStorage.clear();
+
+      window.location.href = '/';
+  }
+
   return (
-    <div>
-    Profile name {user.username} and {user.id} 
-    </div>
+    <Box >
+      <Box  display='flex' justifyContent='space-between'  >
+          <Typography variant='h4' gutterBottom > My Profile </Typography>
+          <Button color='inherit' onClick={logout} >
+              Logout &nbsp; <ExitToApp />
+          </Button>
+          {!favoriteMovies.length 
+            ? <Typography variant='h5' >Add favourates to see here! </Typography>
+            : (<Box > 
+            FAVOURITE MOVIES
+              </Box>)}
+      </Box>
+    </Box>
   );
 };
 
